c6172138b00866e87d4aba4d58e5b59c3e97a3b9
shaix0 <163724966+shaix0@users.noreply.github.com>
2025-11-26 23:40:52 +0800

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+// lib/main.dart
+import 'routes.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'app_theme.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  //await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  //final userCredential = await FirebaseAuth.instance.signInAnonymously();
+  //print('匿名使用者登入成功，UID: ${userCredential.user?.uid}');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Auth Demo',
+      theme: AppTheme.theme,
+      initialRoute: '/',
+      routes: appRoutes,
+    );
+  }
+}
