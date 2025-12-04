654f73f3a766403d42b1212a70805c4088d591c6
JiPe <pennywong1101@gmail.com>
2025-12-04 16:50:11 +0800

    12/4 分支確認

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ if not firebase_admin._apps:
 
 app = FastAPI()
 
-// ↓↓↓部署前要改↓↓↓
+# ↓↓↓部署前要改↓↓↓
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
@@ -29,8 +29,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-// ↑↑↑部署前要改↑↑↑
+# ↑↑↑部署前要改↑↑↑
 
 app.include_router(admin_router, prefix="/admin")  # include the user router under /users path
-app.include_router(settings_router, prefix="/settings")  # include the settings router under /settings path
+# app.include_router(settings_router, prefix="/settings")  # include the settings router under /settings path
 
