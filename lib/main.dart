949cf3c49583633ef01b0f542663aa149bbaaa55
ga Naaa <mix9958@gmail.com>
2026-05-04 17:21:35 +0800

    04

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,22 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'app_theme.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'notifications/notification_handler.dart';
+import 'widget_handler.dart';
+import 'package:provider/provider.dart';
+
 // 全局變數，用於儲存當前登入的使用者資訊
 User? currentUser;
 
+// final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 // 負責初始化 Firebase Auth，並確保使用固定的匿名 UID
 Future<void> _initializeAuth() async {
+
   // 1. 檢查是否有現有的使用者登入狀態
   currentUser = FirebaseAuth.instance.currentUser;
 
+  // 2. 如果沒有，則執行匿名登入，並取得新的 UID
   if (currentUser == null) {
-    // 2. 如果沒有，則執行匿名登入，並取得新的 UID
     try {
       final userCredential = await FirebaseAuth.instance.signInAnonymously();
       currentUser = userCredential.user;
@@ -27,7 +33,7 @@ Future<void> _initializeAuth() async {
     }
   } else {
     // 3. 如果有現有使用者，直接使用，確保每次測試 UID 都是固定的
-    print('=== 找到現有使用者，使用舊 UID: ${currentUser!.uid} ===');
+    print('=== 找到現有使用者UID: ${currentUser!.uid} ===');
   }
 }
 
@@ -42,14 +48,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // 2. 載入環境變數 (.env)
-  // 這是修復 NotInitializedError 的關鍵步驟
   try {
-    // 假設您的環境變數檔案名稱為 .env
     await dotenv.load(fileName: ".env");
     print("環境變數載入成功！");
   } catch (e) {
     print("環境變數載入失敗: $e");
-    // 如果這裡失敗，後續用到 dotenv.env 的地方都會出錯
   }
 
   // 3. 初始化 Firebase 核心服務
@@ -61,7 +64,8 @@ Future<void> main() async {
   await NotificationHandler.init();
   // 註冊背景訊息 handler
   FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
-
+  // 6. 桌面小工具初始化
+  WidgetHandler.init();
   //await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   //final userCredential = await FirebaseAuth.instance.signInAnonymously();
   //print('匿名使用者登入成功，UID: ${userCredential.user?.uid}');
@@ -74,10 +78,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Auth Demo',
+      title: 'aiNutrition',
       theme: AppTheme.theme,
       initialRoute: '/',
       routes: appRoutes,
+      navigatorKey: WidgetHandler.navigatorKey, // 設定全局導航鍵
       debugShowCheckedModeBanner: false, // 隱藏右上角的DEBUG標籤
     );
   }
