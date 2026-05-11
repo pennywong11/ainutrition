36e1f4a4ecc44c73481e1c2d3123943620b80b33
zhni05 <lucy097066@gmail.com>
2026-05-12 06:58:13 +0800

    feat:新增report_page.dart、下載功能 (#71)
    * 新增註解、修改部分程式碼
    
    1. 新增註解
    2. 刪減/修改部分程式碼
    
    * 新增註解、修改部分程式碼
    
    1. 新增註解
    2. 修改部分程式碼
    
    * fix: 解決 history_page 衝突
    
    * Feat： 修改電腦與手機版的週報月報佈局。
    
    * feat: 新增report_page.dart、下載匯出PDF功能
    
    ---------
    
    Co-authored-by: zhni05 <lucy097066@gmial.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_core
 import firebase_messaging
 import flutter_local_notifications
 import path_provider_foundation
+import printing
 import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -22,5 +23,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
