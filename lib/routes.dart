ad93eb491a10345eefac9d8e4ffe415329bd9595
JiPe <pennywong1101@gmail.com>
2026-05-13 12:53:05 +0800

    feat: 加入快拍模式

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,6 +4,8 @@ import 'history_page.dart';
 import 'analysisfood.dart';
 import 'auth.dart';
 import 'admin.dart';
+import 'ana.dart';
+import 'ana2.dart';
 import 'settings.dart';
 import 'pages/family_settings_page.dart';
 
@@ -12,6 +14,6 @@ final Map<String, WidgetBuilder> appRoutes = {
   '/auth': (context) => const AuthPage(), // 登入/註冊頁
   '/settings': (context) => const SettingsPage(), // 設定頁
   '/admin': (context) => const AdminPage(), // 管理頁
-  '/analysis': (context) => const DashboardPage(), // 分析頁
+  '/analysis': (context) => const DashboardPage3(), // 分析頁
   '/family_settings': (context) => const FamilySettingsPage(),
 };
